268874617f96641883a3575fc51dac6d4213c39c
Axel Christiant Kusuma <43558387+sirpenguin33@users.noreply.github.com>
2018-11-28 20:39:33 -0800

    haha

--- application/views/Login_Page.php
+++ application/views/Login_Page.php
@@ -4,10 +4,13 @@
 		<meta charset="UTF-8">
 		<title> Login Page</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css')?>" >
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>" >
 
 	</head>
 	<body>
+	<div class="fullscreen-blue">
 	<h1 class="display-1">Welcome to <br> Pedestana</h1>
+	</div>
 	
 	</body>
 </html>
