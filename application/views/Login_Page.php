a08deeebd346b9773599e449c4867b60add2e2ed
Axel Christiant Kusuma <43558387+sirpenguin33@users.noreply.github.com>
2018-11-28 19:56:42 -0800

    tes

---
+++ application/views/Login_Page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title> Login Page</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css')?>" >
+
+	</head>
+	<body>
+	<h1 class="display-1">Welcome to <br> Pedestana</h1>
+	
+	</body>
+</html>
