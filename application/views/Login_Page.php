8a5eb6cec93660f03de7c0b2c71e944a73e9bbf1
Axel Christiant Kusuma <43558387+sirpenguin33@users.noreply.github.com>
2018-11-30 08:07:18 -0800

    login page kurang carousel
    besok lah

--- application/views/Login_Page.php
+++ application/views/Login_Page.php
@@ -5,6 +5,9 @@
 		<title> Login Page</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css')?>" >
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>" >
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.js')?>" ></script>
 	</head>
 	<body style="height: 100%;">
@@ -13,6 +16,42 @@
 			<td style="width:60%;">
 				<div class="container-fluid left-side">
 					<h1 class="display-3">Pedestana</h1>
+	<!--<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+        <img src="<?php echo base_url('assets/img/img1.jpg')?>" alt="Chania" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Chania</h3>
+          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="<?php echo base_url('assets/img/img1.jpg')?>" alt="Chania" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Chania</h3>
+          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+        </div>
+      </div>
+    </div>
+
+    
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>-->
 				</div>
 			</td>
 			<td>
