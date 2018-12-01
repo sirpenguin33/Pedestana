4cbeb329074d3e91b3e66ae38320c11e4ce84e04
RbYudha <38200401+RbYudha@users.noreply.github.com>
2018-12-01 12:03:23 +0700

    YudaAdd2
    - .htacces
    - Boostrap 3.7
    - View : Fasilitator

---
+++ application/views/Layout/Header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PEDESTANA</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap-theme.min.css') ?>">
+</head>
+	<body>
+		<div class= "navbar navbar-default">
+			<div class="container">
+				<h2><span class="glyphicon glyphicon-home"></span>&nbsp;Welcome to PEDESTANA</h2>
+			</div>
+
+
+		<div class="container">
